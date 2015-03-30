4d8abb623a665b559727415cb6a27066c3ce5cf7
Rebecca <rebecca.fisher3@rockets.utoledo.edu>
2015-03-30 14:10:01 -0700

    Lesson 10

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,8 +1,10 @@
 function toggle(element) {
-  var e = document.getElementById(element);
-  if(e.style.display == "block") {
-    e.style.display = "none";
+  var hoverid = document.getElementById(element);
+  if(hoverid.style.display == "block") {
+    hoverid.style.display = "none";
   } else {
-    e.style.display = "block";
+    hoverid.style.display = "block";
   }
 };
+$('#name').tooltip({'trigger':'focus', 'title': "Please enter your name"});
+$('#email').tooltip({'trigger':'focus', 'title': "Questions, comments, concerns? Please leave your email"});
